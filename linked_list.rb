c7983739e89a99200d745d169408606c6c1b5326
Luis Ybarra <lrybarra@gmail.com>
2016-05-13 10:35:55 -0500

    yooooo

--- linked_list.rb
+++ linked_list.rb
@@ -1,5 +1,65 @@
 require_relative 'node'
 
 class LinkedList
+  def initialize
+    @firstNode = nil
+    #@lastNode = nil
+  end
 
+  def insert_first(element)
+    if @firstNode == nil
+      @firstNode = Node.new(element)
+      p "First Node is" + @firstNode.to_s
+    else
+      tempNode = @firstNode
+      @firstNode = Node.new(element)
+      p "First Node is" + @firstNode.to_s
+      p @firstNode
+      @firstNode.insert_after(tempNode)
+    end
+  end
+
+  def remove_first
+    if @firstNode == nil
+    else
+      if @firstNode.nextNode == nil
+        node = @firstNode
+        @firstNode = nil
+        return node
+      else
+        node = @firstNode
+        @firstNode = @firstNode.nextNode
+        return node
+      end
+    end
+  end
+
+  def insert_last(element)
+  end
+
+  def remove_last
+    previousNode = @firstNode
+    currentNode = @firstNode
+    until currentNode.nextNode == nil
+      previousNode = currentNode
+      currentNode = currentNode.nextNode
+    end
+
+    if currentNode == @firstNode
+      @firstNode = nil
+      return currentNode
+    else
+      previousNode.remove_after
+      return currentNode
+    end
+  end
 end
+
+myLinkedList = LinkedList.new
+p myLinkedList.remove_first
+myLinkedList.insert_first("Uber")
+p myLinkedList.remove_first
+myLinkedList.insert_first("Uber")
+myLinkedList.insert_first("Skrr")
+p "Removing First Node!"
+p myLinkedList.remove_first
